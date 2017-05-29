b0962e4ea75e88e8f0e28d1e57bcbc333d6b0746
crucifyml <i@crucifyml.com>
2017-05-30 00:28:25 +0800

    ac

---
+++ 53.maximum-subarray.cpp
@@ -0,0 +1,50 @@
+/*
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray
+ *
+ * Easy (39.33%)
+ * Total Accepted:    191821
+ * Total Submissions: 487740
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ *
+ * Find the contiguous subarray within an array (containing at least one
+ * number) which has the largest sum.
+ *
+ *
+ * For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+ * the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ *
+ *
+ * click to show more practice.
+ *
+ * More practice:
+ *
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ *
+ */
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if (nums.size() <= 0) {
+            return 0;
+        }
+        vector<int> sums(nums);
+        for (int i = 1; i < sums.size(); i++) {
+            sums[i] = sums[i-1] + sums[i];
+        }
+        int result = sums[0];
+        int min_sum = 0;
+        for (int i = 0; i < sums.size(); i++ ) {
+            if (sums[i] - min_sum > result) {
+                result = sums[i] - min_sum;
+            }
+            if (sums[i] < min_sum) {
+                min_sum = sums[i];
+            }
+        }
+        return result;
+    }
+};
